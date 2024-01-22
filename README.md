1d3fee93d4c49a0696aac74d03541a17cba4f727
Ivo Alves <125821506+ivo-2001@users.noreply.github.com>
2024-01-22 08:13:20 +0000

    Add geolocation and weather functionality

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
-# React + Vite
+# Weather App
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Functional Requirements
 
-Currently, two official plugins are available:
+1. Get Data from Weather API:
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+   1. Implement a button to initiate data retrieval.
+   2. Use [Open Weather API](https://openweathermap.org/api) for weather data.
+
+2. Render API Results:
+
+   1. Render obtained data in the console.
+   2. Create div elements to visually display weather information.
+
+Technologies Used
+
+1. React.js
+2. TailwindCSS
+3. Open Weather API
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const apiKey = import.meta.env.VITE_API_KEY;
+
+const App = () => {
+  const [lat, setLat] = useState();
+  const [lon, setLon] = useState();
+
+  useEffect(() => {
+    getWeatherData();
+  }, [lat, lon]);
+
+  async function getWeatherData() {
+    if (lat && lon) {
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+      );
+      const data = await res.json();
+      console.log(data);
+    }
+  }
+
+  function getLocation() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLat(position.coords.latitude);
+      setLon(position.coords.longitude);
+    });
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <main>
+        <button onClick={getLocation}>Get Weather</button>
+      </main>
+    </div>
+  );
+};
 
-export default App
+export default App;
