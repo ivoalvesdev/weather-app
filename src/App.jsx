cbfbb696c95f296b1b00b26e3ce2a45226347c8f
Ivo Alves <125821506+ivo-2001@users.noreply.github.com>
2024-01-24 07:49:48 +0000

    Improve UI with gradients and shadows.

--- src/App.jsx
+++ src/App.jsx
@@ -66,28 +66,28 @@ const App = () => {
   }
 
   return (
-    <div className="bg-sky-200 h-screen">
-      <header className="bg-sky-900 text-white text-center p-5">
-        <h1 className="text-3xl">Weather App</h1>
+    <div className="bg-gradient-to-tr from-75% from-sky-200 to-amber-100 h-screen">
+      <header className="bg-slate-900 text-white text-center p-3">
+        <h1 className="text-2xl">Weather App</h1>
       </header>
       <main className="container mx-auto my-8 max-w-2xl px-5">
         <div className="text-center">
           <button
-            className="bg-sky-500 hover:bg-sky-600 transition-all text-white px-5 py-3 rounded-full mb-5 aspect-square text-xl"
+            className="bg-sky-500 hover:bg-sky-600 transition-all text-white px-5 py-3 rounded-full mb-5 aspect-square text-xl shadow"
             onClick={getLocation}
           >
             <FaLocationCrosshairs />
           </button>
           <form onSubmit={handleSubmit}>
             <input
-              className="px-5 py-3 mb-5 rounded-l-xl"
+              className="px-5 py-3 mb-5 rounded-l-xl shadow"
               type="text"
               placeholder="City name..."
               required
               onChange={(e) => setInputStr(e.target.value)}
             />
             <button
-              className="bg-sky-500 hover:bg-sky-600 transition-all text-white px-5 py-3 mb-5 rounded-r-xl"
+              className="bg-sky-500 hover:bg-sky-600 transition-all text-white px-5 py-3 mb-5 rounded-r-xl shadow"
               type="submit"
             >
               Search
@@ -98,7 +98,7 @@ const App = () => {
         {weather && (
           <div className="mt-5">
             <div className="flex flex-col sm:grid grid-cols-2 gap-5">
-              <div className="bg-sky-100  hover:bg-sky-50 transition-all p-5 rounded-xl sm:aspect-square flex sm:flex-col items-center sm:justify-center gap-5">
+              <div className="bg-sky-100  hover:bg-sky-50 transition-all p-5 rounded-xl sm:aspect-square flex sm:flex-col items-center sm:justify-center gap-5  shadow">
                 <img
                   className="bg-sky-200 rounded-full sm:mb-3"
                   src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
@@ -110,7 +110,7 @@ const App = () => {
                 </div>
               </div>
 
-              <div className="bg-sky-100  hover:bg-sky-50 transition-all p-5 sm:p-10 rounded-xl sm:aspect-square">
+              <div className="bg-sky-100  hover:bg-sky-50 transition-all p-5 sm:p-10 rounded-xl sm:aspect-square shadow">
                 <h3 className="text-xl font-bold flex items-center gap-3">
                   <FaLocationDot />
                   {weather.name}
@@ -123,13 +123,13 @@ const App = () => {
             </div>
             <br />
             <div className="grid grid-cols-2 gap-5">
-              <div className="bg-sky-100  hover:bg-sky-50 transition-all p-5 rounded-xl aspect-square flex flex-col items-center justify-center">
+              <div className="bg-sky-100  hover:bg-sky-50 transition-all p-5 rounded-xl aspect-square flex flex-col items-center justify-center shadow">
                 <div className="text-6xl text-sky-600">
                   <WiHumidity />
                 </div>
                 <b className="text-lg">{weather.main.humidity}%</b>
               </div>
-              <div className="bg-sky-100  hover:bg-sky-50 transition-all p-5 rounded-xl aspect-square flex flex-col items-center justify-center">
+              <div className="bg-sky-100  hover:bg-sky-50 transition-all p-5 rounded-xl aspect-square flex flex-col items-center justify-center shadow">
                 <div className="text-4xl text-slate-600">
                   <FaWind />
                 </div>
